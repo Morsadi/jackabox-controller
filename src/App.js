bfbcdcd89d77a5ac266001b35f1e25fd6ca0031e
Morsadi <badreddine.morsadi@github.com>
2020-07-08 05:40:03 -0700

    test

--- src/App.js
+++ src/App.js
@@ -53,122 +53,110 @@ export default class App extends Component {
 
   addPlayer = () => {
 
-fetch('https://api6.ipify.org?format=json', {
+
+    const { input_room_code, input_name } = this.state;
+    // fetch for device IP
+    fetch('https://api6.ipify.org?format=json', {
       method: 'GET',
       headers: {},
     })
       .then((json) => {
         return json.json();
-      }).then((e)=>{
-        
-        this.setState({
-        loged_in: true,
-        input_name: e.ip
-        })
       })
-    // const { input_room_code, input_name } = this.state;
-    // // fetch for device IP
-    // fetch('https://api6.ipify.org?format=json', {
-    //   method: 'GET',
-    //   headers: {},
-    // })
-    //   .then((json) => {
-    //     return json.json();
-    //   })
-    //   .then((data) => {
-    //     // then store id in the state
-    //     let ip = data.ip;
-    //     // keep just the digits
-    //     const player_id = ip.replace(/\D/g, '');
-    //     // state
-    //     this.setState({
-    //       player_id,
-    //     });
-    //   })
-    //   .then(() => {
+      .then((data) => {
+        // then store id in the state
+        let ip = data.ip;
+        // keep just the digits
+        const player_id = ip.replace(/\D/g, '');
+        // state
+        this.setState({
+          player_id,
+        });
+      })
+      .then(() => {
 
-    //     const { player_id } = this.state;
-    //     const player = { name: input_name, answer: '' };
+        const { player_id } = this.state;
+        const player = { name: input_name, answer: '' };
 
-    //     this.game.once('value', (snap) => {
-    //       const { room_code, players } = snap.val();
+        this.game.once('value', (snap) => {
+          const { room_code, players } = snap.val();
 
-    //       // if the room_code is the same as entered
-    //       if (room_code === input_room_code) {
-    //         let newPlayers = {};
-    //         // if players exist
-    //         if (players) {
-    //           // if the current id exists while the name is wrong, throw the error
-    //           if (
-    //             players[player_id] &&
-    //             players[player_id].name !== input_name
-    //           ) {
-    //             // throw message
-    //             alert(
-    //               'Please, enter the same name you entered at the beginning',
-    //             );
-    //             // if the current id exists while the name is correct, log in
-    //             // this helps if someone disconnected, or refreshed the page
-    //           } else if (
-    //             players[player_id] &&
-    //             players[player_id].name === input_name
-    //           ) {
-    //             // state
-    //             this.setState({
-    //               loged_in: true,
-    //             });
-    //             // if the id doesn't exist but the name does, push the player to the list but add Jr. as a suffix
-    //           } else if (
-    //             !players[player_id] &&
-    //             players[player_id].name === input_name
-    //           ) {
-    //             newPlayers = { ...players };
-    //             // make new players list
-    //             newPlayers[player_id] = {
-    //               name: `${input_name} Jr.`,
-    //               answer: '',
-    //             };
-    //             // firebase
-    //             this.game.update({
-    //               players: newPlayers,
-    //             });
-    //             // state
-    //             this.setState({
-    //               input_name: `${input_name} Jr.`,
-    //               loged_in:true
-    //             });
-    //             // add player if they don't exist
-    //           } else {
-    //             newPlayers = { ...players };
-    //             // make new players list
-    //             newPlayers[player_id] = player;
-    //             // firebase
-    //             this.game.update({
-    //               players: newPlayers,
-    //             });
-    //             // state
-    //             this.setState({
-    //               loged_in: true,
-    //             });
-    //           }
+          // if the room_code is the same as entered
+          if (room_code === input_room_code) {
+            let newPlayers = {};
+            // if players exist
+            if (players) {
+              // if the current id exists while the name is wrong, throw the error
+              if (
+                players[player_id] &&
+                players[player_id].name !== input_name
+              ) {
+                // throw message
+                alert(
+                  'Please, enter the same name you entered at the beginning',
+                );
+                // if the current id exists while the name is correct, log in
+                // this helps if someone disconnected, or refreshed the page
+              } else if (
+                players[player_id] &&
+                players[player_id].name === input_name
+              ) {
+                // state
+                this.setState({
+                  loged_in: true,
+                });
+                // if the id doesn't exist but the name does, push the player to the list but add Jr. as a suffix
+              } else if (
+                !players[player_id] &&
+                players[player_id].name === input_name
+              ) {
+                newPlayers = { ...players };
+                // make new players list
+                newPlayers[player_id] = {
+                  name: `${input_name} Jr.`,
+                  answer: '',
+                };
+                // firebase
+                this.game.update({
+                  players: newPlayers,
+                });
+                // state
+                this.setState({
+                  input_name: `${input_name} Jr.`,
+                  loged_in:true
+                });
+                // add player if they don't exist
+              } else {
+                newPlayers = { ...players };
+                // make new players list
+                newPlayers[player_id] = player;
+                // firebase
+                this.game.update({
+                  players: newPlayers,
+                });
+                // state
+                this.setState({
+                  loged_in: true,
+                });
+              }
               
-    //           return true;
-    //         } else {
-    //           // else make a new list and add the first player and make him VIP
-    //           newPlayers[player_id] = player;
-    //           // firebase
-    //           this.game.update({
-    //             players: newPlayers,
-    //           });
-    //           // state
-    //           this.setState({
-    //             loged_in: true,
-    //             VIP: true
-    //           });
-    //         }
-    //       }
-    //     });
-    //   });
+              return true;
+            } else {
+              // else make a new list and add the first player and make him VIP
+              newPlayers[player_id] = player;
+              // firebase
+              this.game.update({
+                players: newPlayers,
+              });
+              // state
+              this.setState({
+                loged_in: true,
+                VIP: true
+              });
+            }
+          }
+        });
+      });
   };
   render() {
     const { loged_in, input_name, input_room_code, VIP } = this.state;
